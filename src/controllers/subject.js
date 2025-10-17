4c391c20a97272d2b6aeb0692071535dab5ba53c
GARGACOLLA <gargacola@gmail.com>
2025-10-17 00:41:19 -0400

    updated error responces

--- src/controllers/subject.js
+++ src/controllers/subject.js
@@ -8,25 +8,29 @@ async function getSubjects(req, res) {
   const categoryId = req.query?.categoryId
   const { name, skip, limit, sort } = req.query
 
-  if ((skip && Number.isNaN(Number(skip))) || (limit && Number.isNaN(Number(limit)))) {
-    throw createBadRequestError()
+  if (skip && Number.isNaN(Number(skip))) {
+    throw createBadRequestError('skip must be a number')
   }
-
-  if (
-    (sort && typeof sort !== 'string') ||
-    (name && typeof name !== 'string') ||
-    (categoryId && typeof categoryId !== 'string')
-  ) {
-    throw createBadRequestError()
+  if (limit && Number.isNaN(Number(limit))) {
+    throw createBadRequestError('limit must be a number')
+  }
+  if (sort && typeof sort !== 'string') {
+    throw createBadRequestError('sort must be a string')
+  }
+  if (name && typeof name !== 'string') {
+    throw createBadRequestError('name must be a string')
+  }
+  if (categoryId && typeof categoryId !== 'string') {
+    throw createBadRequestError('categoryId must be a string')
   }
 
   const match = getMatchOptions({ category: categoryId, name: getRegex(name) })
   const sortOptions = getSortOptions(sort)
 
-  const pageNum = skip ? parseInt(skip, 10) : null
+  const skipNum = skip ? parseInt(skip, 10) : null
   const limitNum = limit ? parseInt(limit, 10) : null
 
-  const resources = await subjectService.getSubjects(match, sortOptions, pageNum, limitNum)
+  const resources = await subjectService.getSubjects(match, sortOptions, skipNum, limitNum)
 
   res.status(200).json(resources)
 }
@@ -35,7 +39,7 @@ async function getSubjectsNames(req, res) {
   const categoryId = req.query?.categoryId
 
   if (categoryId && typeof categoryId !== 'string') {
-    throw createBadRequestError()
+    throw createBadRequestError('categoryId must be a string')
   }
 
   const match = getMatchOptions({ category: categoryId })
