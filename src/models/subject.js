4945c59aa2bfd93d76da8d512d1901a1d11ddf63
GARGACOLLA <gargacola@gmail.com>
2025-11-03 01:36:57 -0500

    corrected reference in subject model and docs

--- src/models/subject.js
+++ src/models/subject.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const SubjectSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'ResourcesCategory', required: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
     totalOffers: {
       student: { type: Number, default: 0, min: 0 },
       tutor: { type: Number, default: 0, min: 0 }
